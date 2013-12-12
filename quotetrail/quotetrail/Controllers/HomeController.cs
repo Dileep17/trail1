a2af4eaec40466815b40527412675134731262dc
unknown <dileep@INDileep.corporate.thoughtworks.com>
2013-12-12 10:56:04 +0530

    Initial commit <Dileep>

---
+++ quotetrail/quotetrail/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace quotetrail.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [Authorize]
+        public ActionResult Home()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Models.User User,string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (HttpContext.User.Identity.IsAuthenticated)
+                {
+                    return View(returnUrl);
+                }
+                if (User.IsValid(User.UserName))
+                {
+                    FormsAuthentication.SetAuthCookie(User.UserName, User.RememberMe);
+                    return RedirectToAction("Home", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Login data is incorrect!");
+                }
+            }
+            return View(User);
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
+    }
+}
---
+++ quotetrail/quotetrail/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+
+namespace quotetrail.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Remember on this computer")]
+        public bool RememberMe { get; set; }
+        
+        /// <summary>
+        /// Checks if user with given password exists in the database
+        /// </summary>
+        /// <param name="_username">User name</param>
+        /// <param name="_password">User password</param>
+        /// <returns>True if user exist and password is correct</returns>
+        public bool IsValid(string _username)
+        {
+            using (var cn = new SQLiteConnection(@"Data Source=C:\SQLite\Quotes.db; Version=3;")) 
+            {
+                string _sql = @"SELECT id FROM Users " + 
+                       "WHERE Name = '"+_username+"';";
+                cn.Open();
+                var cmd = new SQLiteCommand(_sql, cn);
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+            }
+        }
+    }
+}
