a542dd2a5f55e60621fcd0e9b6bbb49b03361c02
Dileep <dileep@INDileep.corporate.thoughtworks.com>
2013-12-14 11:53:35 +0530

    Added minification by bundling <Dileep>

---
+++ quotetrail/quotetrail/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace quotetrail.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/content/css/common.css"));
+        }
+    }
+}
--- quotetrail/quotetrail/Global.asax.cs
+++ quotetrail/quotetrail/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+using quotetrail.App_Start;
 
 namespace quotetrail
 {
@@ -19,6 +21,7 @@ namespace quotetrail
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
